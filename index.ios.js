378ee88fe3553c71c8e90e7a652e4fc84ba7e3fd
Sean Pietz <spietz@handybook.com>
2016-04-07 22:35:58 -0400

    add search view

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,8 @@ import React, {
   Component,
   Image,
   ListView,
+  TextInput,
+  TouchableHighlight,
   StyleSheet,
   Text,
   View
@@ -41,9 +43,13 @@ class YTQ extends Component {
       .done();
   }
 
+  hello() {
+    console.log(this.state)
+  }
+
   render() {
     if (!this.state.loaded) {
-      return this.renderLoadingView();
+      return this.renderInitView();
     }
     
     return (
@@ -55,9 +61,26 @@ class YTQ extends Component {
     );
   }
 
-  renderLoadingView() {
+  renderInitView() {
     return (
       <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for an artist or song!
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput style={styles.searchInput}/>
+          <TouchableHighlight style={styles.button} onPress={this.hello}
+              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.rowContainer}>
         <Text>
           Loading movies...
         </Text>
@@ -67,14 +90,13 @@ class YTQ extends Component {
 
   renderSong(song) {
     return (
-      <View style={styles.container}>
+      <View style={styles.rowContainer}>
         <Image
         source={{uri: song.thumbnail}}
           style={styles.thumbnail}
         />
         <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
+          <Text style={styles.title}>{song.title}</Text>
         </View>
       </View>
     );
@@ -82,7 +104,7 @@ class YTQ extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rowContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
@@ -107,6 +129,50 @@ const styles = StyleSheet.create({
   listView: {
     paddingTop: 20,
     backgroundColor: '#F5FCFF'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   }
 });
 
